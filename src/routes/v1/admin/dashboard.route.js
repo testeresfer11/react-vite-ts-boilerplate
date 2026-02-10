49efbb2fa74b3fce5ca8a1ebb4f738f1a366c87c
developer <developer@esferasoft.com>
2026-02-10 16:13:37 +0530

    clear unnecessary codes and files

--- src/routes/v1/admin/dashboard.route.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { countAllRolesUser } = require("../../../controllers/admin.controller/dashboard.controller");
-const auth = require("../../../middlewares/auth");
-
-const dashboardRouter = Router()
-dashboardRouter.use(auth())
-dashboardRouter.route("/count").get(countAllRolesUser)
-
-module.exports  = dashboardRouter
